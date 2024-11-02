99e3782237057274410b53faddc5c57c438b89b0
Highmaster <nzechideraann@gmail.com>
2024-11-02 15:13:15 +0100

    adding the getters and setters

--- src/main/java/model/Receipt.java
+++ src/main/java/model/Receipt.java
@@ -10,4 +10,56 @@ public class Receipt {
     //0 ==> Operatiom
     //1 ===> Report
     private int itemID; //Operation/Report ID
+
+    public Receipt(){
+
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public Cashier getCashier() {
+        return cashier;
+    }
+
+    public void setCashier(Cashier cashier) {
+        this.cashier = cashier;
+    }
+
+    public Patient getPatient() {
+        return patient;
+    }
+
+    public void setPatient(Patient patient) {
+        this.patient = patient;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    public int getItemID() {
+        return itemID;
+    }
+
+    public void setItemID(int itemID) {
+        this.itemID = itemID;
+    }
 }
