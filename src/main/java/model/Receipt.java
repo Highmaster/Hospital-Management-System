856aefdb854d33b886d24906c52a90eec2936e0a
Highmaster <nzechideraann@gmail.com>
2024-11-02 15:09:32 +0100

    adding the receipt class and the private fields

--- src/main/java/model/Receipt.java
+++ src/main/java/model/Receipt.java
@@ -1,4 +1,13 @@
 package model;
 
 public class Receipt {
+
+    private int ID;
+    private Cashier cashier;
+    private Patient patient;
+    private double amount;
+    private int type;
+    //0 ==> Operatiom
+    //1 ===> Report
+    private int itemID; //Operation/Report ID
 }
