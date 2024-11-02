a140c59c5d99b51c4ee237ee176819fc32057662
Highmaster <nzechideraann@gmail.com>
2024-11-02 03:33:45 +0100

    adding the Doctor class

---
+++ src/main/java/model/Doctor.java
@@ -0,0 +1,18 @@
+package model;
+
+public class Doctor extends Employee{
+
+    private String specialization;
+
+    public Doctor(){
+        super();
+        this.options =  new Option[]{};
+    }
+    public String getSpecialization(){
+        return specialization;
+    }
+
+    public void setSpecialization(String specialization) {
+        this.specialization = specialization;
+    }
+}
