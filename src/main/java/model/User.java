966cb019d2e708e949664c1db8d081fd07a6f9a7
Highmaster <nzechideraann@gmail.com>
2024-11-02 02:28:48 +0100

    adding the User class

---
+++ src/main/java/model/User.java
@@ -0,0 +1,61 @@
+package model;
+
+public class User {
+
+    private int ID;
+    private String firstName;
+    private String latName;
+    private String email;
+    private String phoneNumber;
+    private String password;
+    protected Option[] options;
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLatName() {
+        return latName;
+    }
+
+    public void setLatName(String latName) {
+        this.latName = latName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
+
