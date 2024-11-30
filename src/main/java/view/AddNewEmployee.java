0e7c0f06a8749f67cbdf55a8d5b59b859ad58975
Highmaster <nzechideraann@gmail.com>
2024-11-30 22:20:16 +0100

    modified the AddNewEmployee class

--- src/main/java/view/AddNewEmployee.java
+++ src/main/java/view/AddNewEmployee.java
@@ -1,12 +1,11 @@
 package view;
 
-import model.Cashier;
-import model.Database;
-import model.Option;
-import model.User;
+import controller.CreateEmployee;
+import model.*;
 
 import java.sql.SQLOutput;
 import java.util.Scanner;
+
 
 public class AddNewEmployee implements Option {
 
@@ -22,30 +21,52 @@ public class AddNewEmployee implements Option {
         String phoneNumber = s.next();
         System.out.println("Enter Password: ");
         String password = s.next();
-        System.out.println("Confirm password: ");
-        String confirmPassword = s.next();
+        String confirmPassword;
+        do{
+            System.out.println("Confirm password:");
+            confirmPassword = s.next();
+        } while (password.equals(confirmPassword));
         System.out.println("Enter salary (double): ");
-        double Salary = s.nextDouble();
+        double salary = s.nextDouble();
         int job;
         do {
             System.out.println("Enter job title:\n1. Cashier\n2. Receptionist");
             job = s.nextInt();
         } while (job<0 || job>2);
 
+        Employee employee;
+
         switch (job){
         case 1:
-            Cashier cashier = new Cashier();
-            cashier.setFirstName(firstName);
-            cashier.setLastName(lastName);
-            cashier.setEmail(email);
-            cashier.setPhoneNumber(phoneNumber);
-            cashier.setPassword(password);
-            cashier.setSalary(salary);
+            employee = new Cashier();
+            employee.setFirstName(firstName);
+            employee.setLastName(lastName);
+            employee.setEmail(email);
+            employee.setPhoneNumber(phoneNumber);
+            employee.setPassword(password);
+            employee.setSalary(salary);
             break;
         case 2:
+            employee= new Receptionist();
+            employee.setFirstName(firstName);
+             employee.setLastName(lastName);
+            employee.setEmail(email);
+            employee.setPhoneNumber(phoneNumber);
+            employee.setPassword(password);
+            employee.setSalary(salary);
             break;
+            default:
+                employee = new Employee() {
 
+                    @Override
+                    public int getJob() {
+                        return -1;
+                    }
+                };
         }
+
+        if (new CreateEmployee(employee, database).isCreated());
+        System.out.println("Employee added successfully");
 
     }
 
