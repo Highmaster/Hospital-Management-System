e2574df986d65c6f24c11df992ee075be195d8f4
Highmaster <nzechideraann@gmail.com>
2024-11-30 22:18:54 +0100

    modified the reademployee class

--- src/main/java/controller/ReadEmployees.java
+++ src/main/java/controller/ReadEmployees.java
@@ -1,7 +1,9 @@
 package controller;
 
+import model.Cashier;
 import model.Database;
 import model.Employee;
+import model.Receptionist;
 
 import java.sql.ResultSet;
 import java.util.ArrayList;
@@ -15,9 +17,45 @@ public class ReadEmployees {
         String select = "SELECT * FROM 'employees';";
         employees = new ArrayList<>();
         try {
-            ResultSet = database.getStatement().e
+            ResultSet rs = database.getStatement().executeQuery(select);
+            while(rs.next()) {
+                Employee e;
+                int job = rs.getInt("Job");
+                switch (job) {
+                    case 1:
+                        e = new Cashier();
+                        break;
+                    case 2:
+                        e = new Receptionist();
+                        break;
+                    default:
+                        e= new Employee() {
+                            @Override
+                            public int getJob() {
+                                return -1;
+                            }
+                        };
+
+
+                }
+
+               e.setID(rs.getInt("ID"));
+               e.setFirstName(rs.getString("FirstName"));
+               e.setLastName(rs.getString("LastName"));
+               e.setEmail(rs.getString("Email"));
+               e.setPhoneNumber(rs.getString("PhoneNumber"));
+               e.setPassword(rs.getString("Password"));
+               e.setSalary(rs.getDouble("Salary"));
+               employees.add(e);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
 
+    }
+
+    public ArrayList<Employee> getEmployees() {
+        return employees;
     }
 
 
